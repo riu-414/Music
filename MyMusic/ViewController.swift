cfd7ef1b00aee146b5ddaf348b33acd93496b753
riu-414 <mande_e@ezweb.ne.jp>
2022-01-23 16:09:59 +0900

    part1

--- MyMusic/ViewController.swift
+++ MyMusic/ViewController.swift
@@ -6,14 +6,70 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
+    
+    let cymbalPath = Bundle.main.bundleURL.appendingPathComponent("cymbal.mp3")
+    var cymbalPlayer = AVAudioPlayer()
+    
+    let guitarPath = Bundle.main.bundleURL.appendingPathComponent("guitar.mp3")
+    var guitarPlayer = AVAudioPlayer()
+    
+    let backmusicPath = Bundle.main.bundleURL.appendingPathComponent("backmusic.mp3")
+    var backmusicPlayer = AVAudioPlayer()
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func cymbalButton(_ sender: Any) {
+//        do {
+//            cymbalPlayer = try AVAudioPlayer(contentsOf: cymbalPath, fileTypeHint: nil)
+//            cymbalPlayer.play()
+//        } catch {
+//            print("シンバルエラー")
+//        }
+        soundPlayer(player: &cymbalPlayer, path: cymbalPath, count: 0)
+    }
+    
+    @IBAction func guitarButton(_ sender: Any) {
+//        do {
+//            guitarPlayer = try AVAudioPlayer(contentsOf: guitarPath, fileTypeHint: nil)
+//            guitarPlayer.play()
+//        } catch {
+//            print("ギターエラー")
+//        }
+        soundPlayer(player: &guitarPlayer, path: guitarPath, count: 0)
+    }
+    
+    
+    @IBAction func playButton(_ sender: Any) {
+//        do {
+//            backmusicPlayer = try AVAudioPlayer(contentsOf: backmusicPath, fileTypeHint: nil)
+//            backmusicPlayer.numberOfLoops = -1
+//            backmusicPlayer.play()
+//        } catch {
+//            print("プレイエラー")
+//        }
+        soundPlayer(player: &backmusicPlayer, path: backmusicPath, count: -1)
+    }
+    
+    @IBAction func stopButton(_ sender: Any) {
+        backmusicPlayer.stop()
+    }
+    
+    fileprivate func soundPlayer(player:inout AVAudioPlayer, path: URL, count: Int) {
+        do {
+            player = try AVAudioPlayer(contentsOf: path, fileTypeHint: nil)
+            player.numberOfLoops = count
+            player.play()
+        } catch {
+            print("エラー")
+        }
+    }
+    
 }
 
